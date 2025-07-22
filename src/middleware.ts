3bebc06c137a7c724345922fc2819cbaf3c193e7
mstrpyyy <aryahanifn@gmail.com>
2025-07-22 11:49:37 +0700

    debug build middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images(?:/.*)?|fonts|login|$).*)',
+    '/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images(?:/.*)?|fonts|login|logo(?:\\.svg)?|$).*)',
   ],
 };
